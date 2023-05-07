edd7bdb557ba437d6c157249722f7a6c19b750ea
Sinny1s <cry1s@yandex.kz>
2023-05-07 16:42:37 +0000

    problem making!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -23,17 +25,20 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     if let Some(Ok(request_line)) = buf_reader.lines().next() {
-        let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-            ("HTTP/1.1 200 OK", "hello.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        let (status_line, filename) = match &request_line[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            "GET /sleep HTTP/1.1" => {
+                thread::sleep(Duration::from_secs(5));
+                ("HTTP/1.1 200 OK", "hello.html")
+            }
+            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
         };
         let contents =
             fs::read_to_string(filename).unwrap_or("Error: file doesn't exist!".to_string());
         let length = contents.len();
 
         let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
-        
+
         match stream.write_all(response.as_bytes()) {
             Ok(_) => (),
             Err(e) => eprintln!("Something bad happened! {}", e),
